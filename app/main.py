9024aa0273d3496a0ffcfdf2aac49ed4df876f71
Maria Fernanda Orozco <marifeorozco.dom@gmail.com>
2022-03-30 22:47:09 -0500

    Se lee documento de excel. Se valida el estado de cada registro.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,45 @@
-from asyncore import read
+#Sección de importación de librerías.
 import pandas as pd
 
-def readExcel():
+def readExcel() -> list:
     """
-    readExcel():
-    #TODO Upper, lower dictionary keys.
-    #TODO State registers.
+    readExcel(): Método encargado de leer el documento de excel, y convertir los registros en 
+    una lista de diccionarios.
     """
-    df = pd.read_excel('data.xlsx', index_col = None)            
-    dictionary = df.to_dict();                       
-    print(dictionary['FECHA'][1])
+    #Leer excel como dataframe.
+    df = pd.read_excel('dataCopy.xlsx', index_col = None)
+    #Extraer columnas.
+    columns = [key.lower() for key in df.columns]
+    #Extraer registros de factura.
+    listaFacturas = df.values.tolist()
+    #Convertir dataframe a una lista de diccionarios.
+    facturas = [{colum:factura[columns.index(colum)] for colum in columns} for factura in listaFacturas]
+    return facturas
 
-def main():    
-    readExcel()
+def validateState(facturas: list) -> list:
+    """
+    validateState(): Método encargado de validar el estado de cada registro de la lista de facturas.
+    """
+    facturasEnviar = [factura for factura in facturas if factura['estado'].lower() != 'enviado']
+    return facturasEnviar
+
+def sendInvoice():
+    """
+    sendInvoice(): Método encargado de enviar a la API de Alegra las facturas.
+    """    
+
+def changeState():
+    """
+    changeState(): Método encargado de cambiar el estado de cada registro de factura a Enviado.
+    """
+
+
+def main():
+    """
+    main(): Método principal.
+    """
+    facturas = readExcel()
+    facturasEnviar = validateState(facturas = facturas)
 
 if __name__ == '__main__':
     main()
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,2 @@
-a = {"a":1,
-"b":2,
-"c":3,
-"b":4
-}
+import requests
 
-for key,value in a.items():
-    print(key, value)
