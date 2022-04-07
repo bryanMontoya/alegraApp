a55f9c8977cdb80181d81f818c2463a52719b007
Maria Fernanda Orozco <marifeorozco.dom@gmail.com>
2022-04-06 22:59:02 -0500

    Manejo de archivo de excel. Facturas enviadas a nueva hoja. Llamado a la Api con datos por defecto.

--- app/main.py
+++ app/main.py
@@ -1,45 +1,91 @@
 #Sección de importación de librerías.
 import pandas as pd
+import json
+import requests
 
 def readExcel() -> list:
     """
-    readExcel(): Método encargado de leer el documento de excel, y convertir los registros en 
-    una lista de diccionarios.
+    readExcel(): Método encargado de leer el documento de excel, y convertir los registros en
+        una lista de diccionarios.
+
+    Params:
+    None
+
+    Return type is list.
+
     """
-    #Leer excel como dataframe.
-    df = pd.read_excel('dataCopy.xlsx', index_col = None)
-    #Extraer columnas.
-    columns = [key.lower() for key in df.columns]
-    #Extraer registros de factura.
-    listaFacturas = df.values.tolist()
+    #Leer excel como dataframe, extraer columnas y registros.
+    df = pd.read_excel('data - copia.xlsx', index_col = None, sheet_name = "Pendientes")    
+    columns = [key.lower() for key in df.columns]        
+    listInvoices = df.values.tolist()    
+
     #Convertir dataframe a una lista de diccionarios.
-    facturas = [{colum:factura[columns.index(colum)] for colum in columns} for factura in listaFacturas]
-    return facturas
+    invoices = [{colum:factura[columns.index(colum)] for colum in columns} for factura in listInvoices]    
 
-def validateState(facturas: list) -> list:
-    """
-    validateState(): Método encargado de validar el estado de cada registro de la lista de facturas.
-    """
-    facturasEnviar = [factura for factura in facturas if factura['estado'].lower() != 'enviado']
-    return facturasEnviar
+    return invoices
 
-def sendInvoice():
+def sendInvoice(pendingInvoices : list):
     """
     sendInvoice(): Método encargado de enviar a la API de Alegra las facturas.
-    """    
 
-def changeState():
+    Params:
+        list pendingInvoices: Facturas pendientes por enviar.
+    
+    Return type is None    
     """
-    changeState(): Método encargado de cambiar el estado de cada registro de factura a Enviado.
-    """
+    payload = {
+            "date": "2015-11-15",
+            "dueDate": "2015-12-15",
+            "client":  1,
+            "items" : [
+            {
+                "id": 1,
+                "price" : 120,
+                "quantity" : 5    
+            }
+            ]
+        }
+    headers = {
+            "Authorization" : 
+            "Basic Ym1vbnRveWFvc29yaW9zQGdtYWlsLmNvbTo5MzY1NGNlYjA2NTZlZjRlZThkNg=="
+            }      
 
+    for invoice in pendingInvoices:                                                  
+        try:
+            #request = requests.post(url = "https://api.alegra.com/api/v1/invoices",
+            #            headers = headers, data = json.dumps(payload))
+            #if request.status_code == 200:
+                saveInvoice(invoice = invoice)            
+        except:
+            ...
+
+def saveInvoice(invoice : dict):
+    """
+    saveInvoice(): Método encargado de apilar factura enviada en nueva hoja de excel de 
+    nombre 'Facturados'.
+
+    Params:
+        dict invoice: Factura a guardar.
+
+    Return type is None.
+
+    """
+    df = pd.read_excel('data - copia.xlsx', index_col = None, sheet_name = 'Facturados')    
+    df.loc[len(df.values) + 1] = invoice.values()
+    with pd.ExcelWriter('data - copia.xlsx', engine = 'openpyxl', mode ='a', if_sheet_exists = 'replace') as writer:
+        df.to_excel(writer, 'Facturados', index = False)
 
 def main():
     """
-    main(): Método principal.
+    main(): Método principal.    
     """
-    facturas = readExcel()
-    facturasEnviar = validateState(facturas = facturas)
+    pendingInvoices = readExcel()        
+    sendInvoice(pendingInvoices = pendingInvoices)
 
 if __name__ == '__main__':
     main()
+
+#TODO factura: list or dic.
+#TODO: Creación de cliente en alegra.
+#TODO: Creación de producto en alegra.
+#TODO: Delete invoice from Pendientes when sending.
--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,42 @@
-import requests
+"""
+import  pandas  as pd
+from pandas import DataFrame
+ 
+#Leer
+data = pd.read_excel('dataCopy.xlsx')
+ #Ver todos los valores
+print(data.values)
+ #Ver el valor de la primera fila
+print(data.values[0])
+ 
+#Ver todos los valores en una columna
+print(data['Estado'].values)
+ 
+#Añadir columna
+print("-----------------------")
+ #Añadir fila
+data.loc[2,'Estado'] = 'Enviado'
+data.loc[3,'Estado'] = 'Por enviar 2'
+ 
+ #Salvar
+DataFrame(data).to_excel('1.xlsx', sheet_name='Sheet1', index=False, header=True)
 
+
+a = ['a' ,'b' ,'c' , 'd']
+
+for numcol, col in enumerate(a):
+    print(numcol, col)
+    
+import requests
+import base64
+import json
+
+message = "bmontoyaosorios@gmail.com:93654ceb0656ef4ee8d6"
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
+
+print(base64_message)
+"""
+import pandas as pd
+df = pd.read_excel('data - copia.xlsx', index_col = None, sheet_name = "Pendientes")  
