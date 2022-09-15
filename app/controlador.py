41abb3159931242e12b60e1f1d26758d5cfe6d8c
Brayan Montoya osorio <bmontoyaosorios@gmail.com>
2022-09-15 00:49:38 -0500

    Tolerancia a fallos por getProductId. Mensajes de informacion.

--- app/controlador.py
+++ app/controlador.py
@@ -12,51 +12,64 @@ def procesarEnviables(conjuntoRegistros):
         Params: 
         Retorna payload con enviable construido.
         """
-        api = AlegraApi.Api()        
+        api = AlegraApi.Api()
         api.setHeaders(utilsGenHeaders.genBasicToken())
         api.setUrlApi(utils.urlApi)
 
-        registroPrincipal = conjuntoRegistros[0]        
+        registroPrincipal = conjuntoRegistros[0]
         
         try:
-            idCliente = api.getClientById(identification = registroPrincipal['clienteid'])        
+            idCliente = api.getClientById(identification = registroPrincipal['clienteid'])
         except:
-            print("Error consultando la informacion del cliente. " + 
-                    "Verifique que esta identificación " + str(registroPrincipal['clienteid']) + 
-                    " se encuentre asociada a un cliente en Alegra.")
-        else:            
+            print("Error consultando la informacion del cliente. " +
+                    "Valide que la identificación número " + str(registroPrincipal['clienteid'])
+                    + ", se encuentre asociada a un cliente registrado en Alegra.")
+        else:
             payload = {
                 'date': str(registroPrincipal['fecha'].date()),
                 'dueDate': str(registroPrincipal['fechavencimiento'].date()),
-                'client': idCliente,            
+                'client': idCliente,
                 'termsConditions' : utils.terminosCondiciones()
             }
-
+            
+            falloProducto = False
             items = []
             for registro in conjuntoRegistros:
-                idProducto = api.getProductById(referenciaProd = registro['referencia'])        
-                item = {
-                    'id': idProducto,
-                    'price': registro['precio'],
-                    'quantity': registro['cantidad'],   
-                    'reference': registro['referencia']
-                }            
-                items.append(item)                              
-            payload['items'] = items                
+                try:
+                    idProducto = api.getProductById(referenciaProd = registro['referencia'])
+                except:
+                    print("Error consultando informacion del producto." +
+                            " Valide que la referencia número " + str (registro['referencia'])
+                            + ", se encuentre asociada a un producto registrado en Alegra.")
+                    falloProducto = True
+                    break
+                else:
+                    item = {
+                        'id': idProducto,
+                        'price': registro['precio'],
+                        'quantity': registro['cantidad'],
+                        'reference': registro['referencia']
+                    }
+                    items.append(item)
+            
+            #No falla consultando la informacion de ningun producto.
+            if not(falloProducto):
+                payload['items'] = items
+                #Para campos que no se comparten.
+                if registroPrincipal['fact / remis'].lower() == 'facturado':
+                    payload['anotation'] = """Favor consignar en la cta de ahorros Bancolombia #412 
+                            00 00 0219 o en cta ahorros Davivienda #39 4000 054 707 a nombre de Samuel Rendon SAS.""", #TODO Anotaciones en txt
+                    print("Enviando Factura a la Api de Alegra.")
+                    respuesta = api.enviarFactura(payload)
+                elif registroPrincipal['fact / remis'].lower() == 'remisionado':
+                    payload['anotation'] = str(registroPrincipal['transportadora']) + ' ' + str(registroPrincipal['guia']) + '*' + str(registroPrincipal['numeropaquetes'])
+                    print("Enviando Remision a la Api de Alegra.")
+                    respuesta = api.enviarRemision(payload)
 
-            #Para campos que no se comparten.
-            if registroPrincipal['fact / remis'].lower() == 'facturado':
-                payload['anotation'] = """Favor consignar en la cta de ahorros Bancolombia #412 
-                        00 00 0219 o en cta ahorros Davivienda #39 4000 054 707 a nombre de Samuel Rendon SAS.""", #TODO Anotaciones en txt
-                respuesta = api.enviarFactura(payload)
-            elif registroPrincipal['fact / remis'].lower() == 'remisionado':
-                payload['anotation'] = str(registroPrincipal['transportadora']) + ' ' + str(registroPrincipal['guia']) + '*' + str(registroPrincipal['numeropaquetes'])
-                respuesta = api.enviarRemision(payload)
-
-            print(respuesta)                    
+                print(respuesta)
         
     #TODO status code respuesta api.
-'''if response.status_code == 201:                        
+'''if response.status_code == 201:
         excel = modelo.archivoExcel(pathExcel = utils.pathExcelFile)
         excel.guardarFacturados(registro = registro)
         return regIndx     '''
@@ -65,6 +78,7 @@ def procesarConjuntos(registrosPendientes, registrosVaciosIndex):
     """procesarConjuntos: Identificar productos que pertenecen a un mismo cliente.
     Debido a la estructura del archivo de excel, donde para un cliente se apilan los diferentes productos.
     """
+    print("Generando estructura para facturas y remisiones.")
     conjunto = []
     for i, j in enumerate(registrosPendientes):
         if (i not in registrosVaciosIndex):
@@ -83,6 +97,7 @@ def procesarConjuntos(registrosPendientes, registrosVaciosIndex):
 def main():
     
     excelEnviables = modelo.archivoExcel(pathExcel = utils.pathExcelFile)
+    print("Leyendo archivo de Excel.")
     registrosPendientes, registrosVacios = excelEnviables.leerRegistrosPendientes()    
     procesarConjuntos(registrosPendientes, registrosVacios)         
 
