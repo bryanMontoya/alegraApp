34018ebbe819a963976a7b5dabcb9f7a21bdefb5
Brayan Montoya <bmontoyaosorios@gmail.com>
2022-11-22 21:04:31 -0500

    Cambio en distribucion carpetas.

--- app/tokenGenerator.py
+++ app/tokenGenerator.py
@@ -1,3 +1,5 @@
+"""Porción de código que se puede usar para generar token manualmente y poderlo usar en una herramienta como postman."""
+
 import base64
 config = 'correo3@gmail.com' + ":" + '27ab0f21365573156b0e'
 BASICTOKEN = base64.b64encode(config.encode('ascii')).decode('ascii')
